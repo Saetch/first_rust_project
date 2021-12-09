11f5fa76c9d21ae1b2aee10fc2bc4534dcc11701
Saetch <nils.wriedt@hotmail.com>
2021-12-09 01:36:33 +0100

    added windows Message box

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use windows::{
+    core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
+    Win32::UI::WindowsAndMessaging::*,
+};
 
+fn main() -> Result<()> {
+    unsafe{
+        MessageBoxA(None, "Text", "Caption", MB_OK);
+      
+    }  
 
-
-fn main() {
-    println!("Hello, world!");
+    Ok(())
 }
