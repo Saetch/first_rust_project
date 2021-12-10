28671f27ee6cf7838ee09c8f9e82a415cc3c0181
Saetch <nils.wriedt@hotmail.com>
2021-12-10 15:22:33 +0100

    init

---
+++ exercises/clippy/target/debug/deps/clippy2-77b1bd8e45c0efe0.d
@@ -0,0 +1,7 @@
+C:\Users\nilsw\rustlings\rustlings\exercises\clippy\target\debug\deps\clippy2-77b1bd8e45c0efe0.rmeta: clippy2.rs
+
+C:\Users\nilsw\rustlings\rustlings\exercises\clippy\target\debug\deps\clippy2-77b1bd8e45c0efe0.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
